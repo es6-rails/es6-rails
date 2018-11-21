3ad686b49563c8f3cfcbc62a1db2a7fb446bb1fa
Cristian Dugacicu <cristian.dugacicu@external.takeaway.com>
2018-11-21 17:55:30 +0100

    adds es6.js handlers

--- lib/es6/rails/railtie.rb
+++ lib/es6/rails/railtie.rb
@@ -7,13 +7,16 @@ module Es6
   module Rails
     class Railtie < ::Rails::Railtie
       config.app_generators.javascript_engine :es6
+      config.app_generators.javascript_engine :"es6.js"
 
       if config.respond_to?(:annotations)
         config.annotations.register_extensions("es6") { |annotation| /#\s*(#{annotation}):?\s*(.*)$/ }
+        config.annotations.register_extensions("es6.js") { |annotation| /#\s*(#{annotation}):?\s*(.*)$/ }
       end
 
       initializer 'override js_template hook' do |app|
-        if app.config.generators.rails[:javascript_engine] == :es6
+        if app.config.generators.rails[:javascript_engine] == :es6 ||
+          app.config.generators.rails[:javascript_engine] == :"es6.js"
           ::Rails::Generators::NamedBase.send :include, Es6::Rails::JsHook
         end
       end
--- lib/es6/rails/version.rb
+++ lib/es6/rails/version.rb
@@ -1,5 +1,5 @@
 module Es6
   module Rails
-    VERSION = "0.2.0"
+    VERSION = "0.3.1"
   end
 end
